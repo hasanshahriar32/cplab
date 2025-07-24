a6102437997333a0e382da33bda885363ea080bd
Mr. Hasan <hasanshahriar32@gmail.com>
2025-07-24 23:06:48 +0600

    refactor: Move PWA configuration to a separate variable for improved readability

--- next.config.mjs
+++ next.config.mjs
@@ -16,28 +16,26 @@ const nextConfig = {
   experimental: {
     reactCompiler: false,
   },
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'offlineCache',
-          expiration: {
-            maxEntries: 200,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-          },
-        },
-      },
-    ],
-    buildExcludes: [/middleware-manifest.json$/],
-    scope: '/',
-    sw: 'sw.js',
-  },
 }
 
-export default withPWA(withPayload(nextConfig))
+const pwaConfig = {
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'offlineCache',
+        expiration: {
+          maxEntries: 200,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
+      },
+    },
+  ],
+}
+
+export default withPWA(pwaConfig)(withPayload(nextConfig))
